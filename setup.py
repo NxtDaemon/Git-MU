90b637b948b6038f6bb2050b7129df645eb1be0f
Luke Aaron Cotterell <nxtdaemon@DaemonVM>
2021-08-16 01:02:25 +0100

    added conditional setup

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import json
 # Locate seems to be Being a bitch to work with so im not gonna use python to directly process the commmand
 
 PathsArray = []
-
+# Given that some people are highly organised within their environment they likely have a directory in which all git repos are stored the use of the condition variable allows a quick and simple usage of such
+Condition = None 
 
 os.system("locate '*/.git' > .TempCommandOutput")
 
@@ -15,8 +16,15 @@ if ".TempCommandOutput" in os.listdir(os.getcwd()):
         lines = f.readlines()
     
     for Value in lines:
-        PathsArray.append(Value.strip("\n").strip(".git"))
-        
+        Value = Value.strip("\n").strip(".git")
+        if not Condition:
+            PathsArray.append(Value)
+        else:    
+            # Add your Condtion Evaluation Here (E.G. "if Value.startswith('/opt/')" " 
+            # ... 
+            # ...
+            PathsArray.append(Value)
+            
     JSON = {
         "Paths" : PathsArray
     }
